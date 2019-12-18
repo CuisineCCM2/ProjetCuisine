f289e43b1d93a69866dbf0429ed49de225399b12
Paul <paul-olivier.haye@hostabee.com>
2019-12-18 16:06:30 +0100

    cooking api http get

--- app/src/main/java/perso/myapplicationL/ui/main/FirebaseConnection.java
+++ app/src/main/java/perso/myapplicationL/ui/main/FirebaseConnection.java
@@ -96,31 +96,4 @@ public class FirebaseConnection {
             }
         });
     }
-
-//    private static void getAllRecipes() {
-//        dbUser.collection("recettes").get()
-//            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//                @Override
-//                public void onSuccess(QuerySnapshot documentSnapshots) {
-//                    if (documentSnapshots.isEmpty()) {
-//                        Log.d("LISTRECIPES", "onSuccess: LIST EMPTY");
-//                        return;
-//                    } else {
-//                        // Convert the whole Query Snapshot to a list
-//                        // of objects directly! No need to fetch each
-//                        // document.
-//                        List<Type> types = documentSnapshots.toObjects(Type.class);
-//
-//                        // Add all to your list
-//                        listRecipes.addAll(types);
-//                        Log.d("LISTRECIPES", "onSuccess: " + listRecipes);
-//                    }
-//                }
-//            }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Log.d("LISTRECIPES", "Error getting data!!!");
-//            }
-//        });
-//    }
 }
