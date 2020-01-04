9699916df9c5eafc5e2ac8316fb5c6fae406254f
Victor <victorulysse.piot@gmail.com>
2020-01-04 17:32:24 +0100

    add recipes page & deconnexion

---
+++ app/src/main/java/perso/myapplicationL/AddRecipes.java
@@ -0,0 +1,14 @@
+package perso.myapplicationL;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipes);
+    }
+}
--- app/src/main/java/perso/myapplicationL/Settings.java
+++ app/src/main/java/perso/myapplicationL/Settings.java
@@ -7,14 +7,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    private Button btnaddrecipes, btndeconnexion ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btnaddrecipes = findViewById(R.id.btnaddrecipe);
+        btndeconnexion = findViewById(R.id.btn_deconnexion);
+
+        btnaddrecipes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.this, AddRecipes.class);
+                startActivity(intent);
+            }
+        });
+
+        btndeconnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -26,4 +50,6 @@ public class Settings extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
+
+
 }
